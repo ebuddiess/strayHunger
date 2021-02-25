0e0b663bf44903f3aef33868409cf2b6418edde3
Puneet <ebuddiess@gmail.com>
2021-02-25 20:25:57 +0530

    IntroScreen Attached

--- lib/utility/color_manager.dart
+++ lib/utility/color_manager.dart
@@ -3,4 +3,17 @@ import 'package:flutter/material.dart';
 class ColorManager {
   static Color textcolorwhite = Colors.white;
   static Color textcolorblack = Colors.black;
+
+  static final kTitleStyle = TextStyle(
+    color: Colors.white,
+    fontFamily: 'CM Sans Serif',
+    fontSize: 26.0,
+    height: 1.5,
+  );
+
+  static final kSubtitleStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 18.0,
+    height: 1.2,
+  );
 }
